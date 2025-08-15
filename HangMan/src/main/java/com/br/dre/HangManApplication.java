3e3127755be7813ed1b5573df118e157f1cf9f74
Anderson Freires <dre.son36@gmail.com>
2025-08-15 09:57:01 -0300

    Hangman game in Java using object orientation.

---
+++ HangMan/src/main/java/com/br/dre/HangManApplication.java
@@ -0,0 +1,13 @@
+package com.br.dre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangManApplication.class, args);
+	}
+
+}
